ddfa5046301f9f8716f2ed6e001e18df58bd43b6
wbp <wbpiggot@gmail.com>
2016-05-30 14:48:50 -0700

    Setting up initial external API request

--- app/frontend/javascripts/app/App.js
+++ app/frontend/javascripts/app/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
+import 'whatwg-fetch';
+import 'babel-polyfill';
+
+const HEADERS = {
+  'X-Auth-Token': '663528934d21408f8a4a638a6511cdb5'
+};
 
 
 class App extends Component {
+  constructor() {
+    super(...arguments)
+    this.state = {
+      fixtures: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://api.football-data.org/alpha/teams/57/fixtures', { headers: HEADERS })
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({ fixtures: responseData})
+      })
+  }
+
   render() {
+    console.log(this.state.fixtures)
     return <h1>Hello World</h1>
   }
 }
