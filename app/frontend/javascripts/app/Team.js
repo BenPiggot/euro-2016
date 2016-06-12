59269c253c4b44ff29b0a17e3f5655557d4d5781
wbp <wbpiggot@gmail.com>
2016-06-11 23:34:56 -0700

    Adding TeamInfo Component

--- app/frontend/javascripts/app/Team.js
+++ app/frontend/javascripts/app/Team.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import TeamInfo from './TeamInfo.js';
 
 class Team extends Component {
+  constructor() {
+    super(...arguments)
+    this.state = {
+      showInfo: false
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault()
+    this.setState({ showInfo: !this.state.showInfo })
+    $('body').on('click', () => {
+      this.setState({ showInfo: !this.state.showInfo })
+    })
+  }
 
   render() {
+    const props = {
+      name: this.props.teamName
+    }
     return <tr>
-      <td><img style={{height: '20px', width: '30px'}}src={this.props.image}/></td>
+      <td><img style={{height: '20px', width: '30px'}} src={this.props.image}/></td>
       <td onClick={this.handleClick}>
         {this.props.teamName}
+        {this.state.showInfo ? <TeamInfo {...props} /> : null }
       </td>
       <td>{this.props.played}</td>
       <td>{this.props.goals}</td>
---
+++ app/frontend/javascripts/app/TeamInfo.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+
+class TeamInfo extends Component {
+  constructor() {
+    super(...arguments)
+  }
+
+  render() {
+    return <ReactCSSTransitionGroup>
+      <div className='TeamInfo'>
+        <h1 className='TeamInfo__TeamName'>{this.props.name}</h1>
+      </div>
+    </ReactCSSTransitionGroup>
+  }
+}
+
+export default TeamInfo;
